9f4b49e6aceeec29691ef1a83ecb3d6da2a3c03b
Anish Shrestha <anish08shrestha@gmail.com>
2024-08-02 12:06:35 +0545

    refact : image preview in profile

--- src/user/pages/profile/Profile.jsx
+++ src/user/pages/profile/Profile.jsx
@@ -215,58 +215,65 @@ const Profile = () => {
                   <div className="flex items-center justify-start mt-2 w-full">
                     <label
                       htmlFor="dropzone-file"
-                      className="flex flex-col items-center justify-center w-64 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                      className="flex flex-col items-center justify-center w-64 h-80 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 relative"
                     >
-                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                        <svg
-                          className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 20 16"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                          />
-                        </svg>
-                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                          <span className="font-semibold">Click to upload</span>{" "}
-                          or drag and drop
-                        </p>
-                        <p className="text-xs text-gray-500 dark:text-gray-400">
-                          SVG, PNG, JPG or GIF (MAX. 800x400px)
-                        </p>
-                      </div>
+                      {imagePreview ? (
+                        <img
+                          src={imagePreview}
+                          alt="Selected Preview"
+                          className="w-full h-full rounded-lg object-cover absolute inset-0"
+                        />
+                      ) : (
+                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                          <svg
+                            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 20 16"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                            />
+                          </svg>
+                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                            <span className="font-semibold">
+                              Click to upload
+                            </span>{" "}
+                            or drag and drop
+                          </p>
+                          <p className="text-xs text-gray-500 dark:text-gray-400">
+                            SVG, PNG, JPG or GIF (MAX. 800x400px)
+                          </p>
+                        </div>
+                      )}
                       <input
                         id="dropzone-file"
                         type="file"
                         className="hidden"
                         onChange={handleFileChange}
                       />
-                      <span className="text-sm text-gray-500 dark:text-gray-400 mt-2">
-                        {fileName ? fileName : "Upload Your Profile Picture"}
-                      </span>
                     </label>
-                    {imagePreview && (
-                      <div className="ml-4">
-                        <img
-                          src={imagePreview}
-                          alt="Selected Preview"
-                          className="w-32 h-32 rounded-lg object-cover"
-                        />
-                      </div>
-                    )}
                   </div>
+                  {fileName && (
+                    <div className="mt-2">
+                      <p className="text-gray-500 dark:text-gray-400">
+                        Selected file: {fileName}
+                      </p>
+                    </div>
+                  )}
                 </div>
               </div>
-              <div className="flex flex-col items-center justify-center  mt-6 md:flex-row">
-                <button className="inline-block w-auto text-center min-w-[200px] px-6 py-4 text-white transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-blue-600 to-blue-500 hover:bg-gradient-to-b dark:shadow-blue-900 shadow-blue-200 hover:shadow-2xl hover:shadow-blue-400 hover:-translate-y-px">
-                  Save Details
-                </button>
+              <div className="mt-10 ml-10">
+                <div className="flex flex-col items-center justify-center mt-6 md:flex-row">
+                  <button className="inline-block w-auto text-center min-w-[180px] px-6 py-4 text-white transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-blue-600 to-blue-500 hover:bg-gradient-to-b dark:shadow-blue-900 shadow-blue-200 hover:shadow-2xl hover:shadow-blue-400 hover:-translate-y-px">
+                    Save Details
+                  </button>
+                </div>
               </div>
             </div>
           </div>
