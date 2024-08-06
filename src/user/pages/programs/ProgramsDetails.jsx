5d0d73268f88ac6fec9cf6269c1f8374e5114a52
Anish Shrestha <anish08shrestha@gmail.com>
2024-08-06 12:24:06 +0545

    sub menu added in program details page

--- src/user/pages/programs/ProgramsDetails.jsx
+++ src/user/pages/programs/ProgramsDetails.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import profile1 from "../../../assets/profile1.jpg";
 
 const ProgramsDetails = () => {
+  const [activeTab, setActiveTab] = useState("Program Details");
+  const tabs = ["Program Details", "Announcements", "Hall of Fame"];
+
   return (
     <div className="bg-gray-900 text-white min-h-screen">
       <div className="flex justify-start ml-32 gap-40">
@@ -13,14 +16,16 @@ const ProgramsDetails = () => {
             Under Armour’s vision is to inspire you with performance solutions
             you never knew you needed and can’t imagine living without.
           </p>
-          <div className="mt-4">
-            <span className="text-primary font-semibold">
+          <div className="mt-4 text-right mb-2 flex justify-end gap-2 items-center">
+            <span className="text-primary font-bold text-xl text-indigo-400">
               Rs 125 - Rs 2,500
             </span>{" "}
-            per vulnerability
+            <span className="font-semibold font-grotesk">
+              per vulnerability
+            </span>
           </div>
           <hr />
-          <button class="bg-blue-500 hover:bg-blue-700 mt-4 text-white font-bold py-2 px-4 rounded">
+          <button className="bg-blue-500 hover:bg-blue-700 mt-4 text-white font-bold py-2 px-4 rounded">
             Submit Report
           </button>
         </div>
@@ -30,6 +35,23 @@ const ProgramsDetails = () => {
             alt="Profile"
             className="w-full h-full object-cover"
           />
+        </div>
+      </div>
+      <div className="bg-card mt-2 ml-32 h-14 bg-gray-700 text-card-foreground p-4 rounded-xl w-[970px]">
+        <div className="flex gap-10 mb-2 font-bold text-lg ml-4 text-gray-400 cursor-pointer">
+          {tabs.map((tab) => (
+            <span
+              key={tab}
+              onClick={() => setActiveTab(tab)}
+              className={`${
+                activeTab === tab
+                  ? "text-indigo-400 underline"
+                  : "text-gray-400"
+              }`}
+            >
+              {tab}
+            </span>
+          ))}
         </div>
       </div>
     </div>
